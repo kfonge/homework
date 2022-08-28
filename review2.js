b9003d551e841b6281a39659ffac5155b451eec6
Drapetes Dandridge <drapetesdandridge@cats-mbp.lan>
2022-08-28 00:08:22 -0400

    start up to fizz buzz complete

---
+++ review2.js
@@ -0,0 +1,52 @@
+////////// Easy Going
+////////////////////////
+
+// for (let i = 0; i <= 20; i++){
+//     console.log(i);
+// }
+
+////////// Get Even///////
+///////////////////////////
+// for (let i = 0; i <= 200; i++){
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+///////////////////
+/////Fizz Buzz
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 != 0) {
+    console.log("Fizz")
+  } else {
+    if (i % 5 == 0 && i % 3 != 0) {
+      console.log("Buzz")
+    } else {
+      if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz")
+      } else {
+        console.log(i)
+      }
+    }
+  }
+}
+///////Wild Wild Life
+//////////////////////
+
+//////Yell at the Ninja Turtles
+///////////
+
+//////Methods, Revisited
+/////////////////
+
+/////////// Where is Waldo
+///////////
+
+//////////////
+///////Excited Kitten
+/////////
+
+//////////////////////
+//////Find the Median
+/////////////////////////////////
